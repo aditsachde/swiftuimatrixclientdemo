69e06d766ba560e103b160d0770597993b50ff86
Adit Sachde <23707194+aditsachde@users.noreply.github.com>
2021-01-12 17:14:07 -0500

    Initial Commit

---
+++ Koven/KovenApp.swift
@@ -0,0 +1,17 @@
+//
+//  KovenApp.swift
+//  Koven
+//
+//  Created by Adit Sachde on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct KovenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
